d89f9d693f0fa50d51fca9add1643ed0a58519ba
wuqian2 <1050570474@qq.com>
2020-03-03 17:25:41 +0800

    Initial commit

---
+++ src/test/java/com/sxnsyh/lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxnsyh.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
